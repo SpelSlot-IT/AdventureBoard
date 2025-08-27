ac5d4920f2d64765ae8a292103f183159c1c6666
Jille Timmermans <jille@quis.cx>
2025-08-27 20:21:47 +0200

    frontend: Show the adventures on the home page

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,6 +1,23 @@
 <template>
   <q-page class="row items-center justify-evenly">
-		Hello {{me.display_name}}
+		<q-card v-for="a in adventures" :key="a.id">
+			<q-card-section>
+				<div class="text-h6">{{a.title}}</div>
+				<q-chip v-for="t in a.tags.split(',')" :key="t" :label="t" color="accent" text-color="white"  />
+				<div>{{a.short_description}}</div>
+				<q-btn label="Details" icon="info" @click="focussed = a" color="primary" />
+			</q-card-section>
+		</q-card>
+
+		<q-dialog :modelValue="!!focussed" @hide="focussed = null">
+			<q-card style="min-width: 300px">
+				<q-card-section>
+					<div class="text-h6">{{focussed.title}}</div>
+					<q-chip v-for="t in focussed.tags.split(',')" :key="t" :label="t" color="secondary" text-color="white"  />
+					<div>{{focussed.short_description}}</div>
+				</q-card-section>
+			</q-card>
+		</q-dialog>
   </q-page>
 </template>
 
@@ -13,6 +30,33 @@ export default defineComponent({
 		return {
 			me: inject('me'),
 		};
+	},
+	data() {
+		let prevMonday = new Date();
+		prevMonday.setDate(prevMonday.getDate() - (prevMonday.getDay() + 6) % 7);
+		return {
+			weekStart: prevMonday.toISOString().split('T')[0],
+			adventures: [],
+			focussed: null as any,
+		};
+	},
+	methods: {
+	},
+	computed: {
+		weekEnd() {
+			const d = new Date(this.weekStart);
+			d.setDate(d.getDate() + 7);
+			return d.toISOString().split('T')[0];
+		},
+	},
+	watch: {
+		weekStart: {
+			async handler() {
+				const resp = await this.$api.get('/api/adventures?week_start=' + this.weekStart + '&week_end=' + this.weekEnd);
+				this.adventures = resp.data;
+			},
+			immediate: true,
+		},
 	},
 });
 </script>
