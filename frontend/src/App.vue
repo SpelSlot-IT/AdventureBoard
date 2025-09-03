d986618fc5d3aaba716d807080baf6fea6427229
Bram <bramstrik@live.com>
2025-09-03 21:07:32 +0200

    frontend: Add custom icon library

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,8 +4,22 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { IconSet } from 'quasar';
+
+const myIcons = {
+  'app:icon1': 'img:/quasar-logo-vertical.svg',
+  'app:icon2': 'img:/icon2.svg',
+  'app:copy': 'fas fa-copy',
+};
+
+IconSet.iconMapFn = (iconName) => {
+  const icon = myIcons[iconName];
+  if (icon !== undefined) {
+    return { icon };
+  }
+};
 
 export default defineComponent({
-  name: 'App'
-})
+  name: 'App',
+});
 </script>
