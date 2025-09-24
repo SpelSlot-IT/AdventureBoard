07054f84d49039120c3d8a8edd3e2a931f88c318
Jorrit van der Heide <jorrit@bw20.nl>
2025-09-24 20:48:36 +0200

    feat: add nix flake

--- flake.nix
+++ flake.nix
@@ -3,33 +3,40 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = {nixpkgs, ...}: let
-    # Define the system architecture
-    system = "x86_64-linux";
-    # system = "x86_64-darwin";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+  outputs =
+    { nixpkgs, ... }:
+    let
+      # Define the system architecture
+      system = "x86_64-linux";
+      # system = "x86_64-darwin";
     in
-      pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs
-        ];
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        pkgs.mkShell {
+          name = "impurePythonEnv";
+          venvDir = "./.venv";
 
-        # Shell hook that executes when the development environment starts
-        shellHook = ''
-          # If the shell is Fish, start a new Fish shell session to ensure proper environment setup
-          if [[ "$SHELL" == *"fish"* ]]; then
-            exec fish
-          fi
+          buildInputs = with pkgs; [
+            nodejs
+            python313
+            python313Packages.venvShellHook
+            sqlite
+          ];
 
-          # If no node_modules folder exists, install the project dependencies
-          if [ ! -d node_modules ]; then
-            npm install
-          fi
-        '';
-      };
-  };
+          postVenvCreation = ''
+            unset SOURCE_DATE_EPOCH
+            cd board && pip install -r requirements.txt
+          '';
+
+          postShellHook = ''
+            # allow pip to install wheels
+            unset SOURCE_DATE_EPOCH
+          '';
+        };
+    };
 }
