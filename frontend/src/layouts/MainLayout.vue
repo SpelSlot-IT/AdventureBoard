0af050f1d63f2275b4ff10f4094a8716bda99529
Bram <bramstrik@live.com>
2025-09-25 12:10:00 +0200

    Frontend: add push notifications permission checks

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -76,9 +76,12 @@
         @finishAdminAction="adminActionsActive--"
       />
       <span v-if="version" class="fixed-bottom-left q-ml-sm">
-        AdventureBoard v{{ version }} 
+        AdventureBoard v{{ version }}
       </span>
-      <a href="https://github.com/SpelSlot/AdventureBoard" class="fixed-bottom-right q-mr-sm">
+      <a
+        href="https://github.com/SpelSlot/AdventureBoard"
+        class="fixed-bottom-right q-mr-sm"
+      >
         <q-icon name="github" size="lg" />
       </a>
     </q-page-container>
@@ -88,6 +91,9 @@
 <script lang="ts">
 import { defineComponent, computed } from 'vue';
 import { isAxiosError } from 'axios';
+import { setupNotifications } from '../util/pushNotifications';
+
+setupNotifications();
 
 export default defineComponent({
   name: 'MainLayout',
@@ -173,7 +179,6 @@ export default defineComponent({
       }
     },
   },
-
   async beforeMount() {
     this.loading = true;
     try {
---
+++ frontend/src/util/pushNotifications.ts
@@ -0,0 +1,61 @@
+async function handleNotifications(): Promise<boolean> {
+    if (Notification.permission === 'denied') {
+        return false;
+    }
+    
+    if (Notification.permission !== 'granted') {
+        const permission = await Notification.requestPermission();
+        return permission === "granted";
+    }
+    return true;
+}
+
+function checkBrowserSupport(): boolean {
+    if (!('Notification' in window)) {
+        return false;
+        // throw new Error('Browser does not support notifications');
+    }    
+    if (!('PushManager' in window)) {
+        return false;
+        // throw new Error('Sorry, Push notification isn\'t supported in your browser.');
+    }
+    if (!('serviceWorker' in navigator)) {
+        return false;
+        // throw new Error('Browser does not support service workers');
+    };
+    return true;
+}
+
+async function subscribePush(registration: ServiceWorkerRegistration) {
+    //Subscribes user to Push notifications
+    const subscription = await registration.pushManager.subscribe({
+        userVisibleOnly: true //Set user to see every notification
+    })
+    console.log(subscription);
+}
+
+export async function setupNotifications() {
+    try {
+        if (!checkBrowserSupport()) {
+            throw new Error('Push notifications arent supported in this browser. Tough luck, bucko.');
+        }
+        const havePermission = await handleNotifications();
+        if (!havePermission) {
+            throw new Error('We don\'t have the required permissions. Not our fault')
+        }
+
+        // register the SW
+        const path = '/js/service-worker.js';
+        await navigator.serviceWorker.register(path);
+        // When it's ready, check if there's a subscription already
+        const registration = await navigator.serviceWorker.ready;
+        const subscription = await registration.pushManager.getSubscription();
+        // If there's not, subscribe to the push notifications
+        if (subscription === null) {
+            subscribePush(registration);
+        }
+        console.log(subscription);
+    } catch (err) {
+        window.alert(err);
+    }
+}
