2561296cda1b184876ef40dbb5cde61c7fd4587c
Jille Timmermans <jille@quis.cx>
2025-10-07 22:16:56 +0200

    frontend: Theoretical fix for linking to characters from /signups

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -6,6 +6,7 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/IndexPage.vue') },
+      // TODO: Put the date in the URL
       { path: 'profile', component: () => import('pages/ProfilePage.vue') },
       { path: 'signups', component: () => import('pages/SignupsPage.vue') },
       {
